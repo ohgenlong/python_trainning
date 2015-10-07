c1c2e03f06fb2d60c4e8614d177b3dd7a813761d
ohgen <ohgenlong@126.com>
2015-10-07 23:12:07 +0800

    process

--- process/multi_process_pool.py
+++ process/multi_process_pool.py
@@ -17,28 +17,28 @@ import multiprocessing
 ## 进程池实例
 ## 控制同时有多少个进程在运行
 
-#def run(num,lock,share_list):
-def run(num,share_list):
+def run(num,lock,share_list):
     time.sleep(1)
-    #lock.acquire()
+    lock.acquire() ## 获取锁
     share_list.append(num)
     print "Add %s to share_list" % num
-    #lock.release()
+    lock.release() ## 释放锁
     return share_list
 
 if __name__=="__main__":
-    l = multiprocessing.Lock()
+
 
     pool = multiprocessing.Pool(processes=5) ## 控制最多同时有5个进程在运行，设置成CPU的核数一样多就ok
-
+    m = multiprocessing.Manager() ## 如要在pool使用锁，必须使用manager方法创建的实例对象中的方法Lock()
+    l = m.Lock()
     ## 创建多进程的中介处理实例
     m = multiprocessing.Manager()
     s_list = m.list()
 
     p_list = []
     for i in range(20):
-        #p = pool.apply_async(run,args=(i,l,s_list,))
-        p = pool.apply_async(run,args=(i,s_list,)) ## 把run函数放到进程池
+        p = pool.apply_async(run,args=(i,l,s_list,)) ## 把run函数放到进程池
+        #p = pool.apply_async(run,args=(i,s_list,)) ## 把run函数放到进程池
         p_list.append(p) ## 启动进程池，并且加入p_list让多进程并发
 
     for p in p_list:
