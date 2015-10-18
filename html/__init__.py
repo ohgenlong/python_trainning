4cc66536f14f4637b6b087c4305b59be2668c891
ohgenlong <ohgenlong@126.com>
2015-10-18 20:24:57 +0800

    select_socket

---
+++ html/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding=UTF-8 -*-
+# author: ohgenlong
+
+import os
+import sys
+import logging
+import ConfigParser
+import time
+import datetime
+import json
+import traceback
+from logging.handlers import RotatingFileHandler
