cd4e0db73cb229a5a851e3720ac953d5eb553e3a
ohgenlong <ohgenlong@126. com>
2015-10-25 19:32:57 +0800

    jquery

---
+++ js/jquery/test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ohgenlong on 2015/10/25.
+ */
+function changeColor(ele){
+    ele.style.background = "yellow";
+    var c = ele.getAttribute('style');
+    var c1 = ele.background;
+    var h = ele.innerHTML;
+    var cc = ele.style.background;
+    console.log(c);
+    console.log(c1);
+    console.log(h);
+    console.log(cc);
+}
